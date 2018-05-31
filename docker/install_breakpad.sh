3e9cbea89a731dbbad293575391d84ed806d4efd
Alexander Drobinsky <alex.drobinsky@gmail.com>
2018-05-31 10:45:22 +0300

    Fixed breakpad installation and changed Makefile of stackwalker accordingly. Removed breakpad from repository.

--- docker/install_breakpad.sh
+++ docker/install_breakpad.sh
@@ -4,25 +4,15 @@ yum -y install git svn hg;
 yum -y install gcc gcc-c++ make;
 
 cd third-party
-#svn co http://google-breakpad.googlecode.com/svn/trunk/ google-breakpad
-#git clone https://github.com/google/breakpad.git
-#git clone https://chromium.googlesource.com/linux-syscall-support breakpad/src/third_party/lss
-#mv breakpad google-breakpad
-mkdir obj-breakpad/
-cd obj-breakpad/ 
-../google-breakpad/configure && make
-#./configure --prefix=/LandingNet/third-party/breakpad && make && make install
-#cp src/processor/minidump_stackwalk /LandingNet/bin/stackwalker
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH=$PATH:/LandingNet/third-party/depot_tools/
+fetch breakpad
+cd src
+./configure && make && make install
 cd ../
-
-
-#hg clone http://hg.mozilla.org/users/tmielczarek_mozilla.com/minidump-stackwalk -b json
 cd minidump-stackwalk
 make
 cp stackwalker ../../bin/
 cd ../../
 
-#wget -N --quiet 'https://index.taskcluster.net/v1/task/project.socorro.breakpad.v1.builds.linux64.latest/artifacts/public/breakpad.tar.gz'
-#tar -zxf breakpad.tar.gz
-#rm -rf stackwalk
-#mv breakpad stackwalk
+
--- third-party/minidump-stackwalk/stackwalker.cc
+++ third-party/minidump-stackwalk/stackwalker.cc
@@ -37,7 +37,6 @@
 
 #include <errno.h>
 #include <getopt.h>
-#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
